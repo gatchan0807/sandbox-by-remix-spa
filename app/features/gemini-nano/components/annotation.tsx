ae2fdaf9629284912ef056f9b68f6b25c38d376e
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-11-14 19:41:59 +0900

    feat: add notice about "Cant use Japanese." on Gemini nano current version

--- app/features/gemini-nano/components/annotation.tsx
+++ app/features/gemini-nano/components/annotation.tsx
@@ -22,6 +22,12 @@ export default function Annotation(props: Props) {
                 </ul>
             </div>
 
+            <p className="bg-red-50 text-red-700 font-bold px-4 py-2 my-4 rounded-md">
+                現在、 Gemini nano in Chrome の仕様上、英語以外を出力しようとするとエラーになります。<br />
+                いずれこれが発生しないようになる予定ですが、2024年11月現在は英語の出力をテストすることしかできません。<br />
+                <a className="underline font-normal" href="https://docs.google.com/document/d/1VG8HIyz361zGduWgNG7R_R8Xkv0OOJ8b5C9QKeCjU0c" target="_blank" rel="noreferrer noopener">最新情報・詳細な仕様はこちらから</a>
+            </p>
+
             <p className="my-4 text-center">
                 <a href="https://note.com/npaka/n/n17176250330e" rel="noreferrer noopener" target="_blank" className="text-blue-600 underline">
                     使えるようにするまでの手順 / How to make it usable
